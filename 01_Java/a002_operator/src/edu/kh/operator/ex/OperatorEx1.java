e5997be0bbc11404455ca874d20ce888c0003da3
ymkim <bfyu@naver.com>
2023-02-08 11:51:30 +0900

    230208_연산자 수업내용

---
+++ 01_Java/a002_operator/src/edu/kh/operator/ex/OperatorEx1.java
@@ -0,0 +1,15 @@
+package edu.kh.operator.ex;
+
+import java.util.Scanner; //Scanner import코드
+
+public class OperatorEx1 {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in); //Scanner 객체 생성
+		
+	
+		
+	}
+
+}
