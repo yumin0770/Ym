6bc98ee4d17e5f55fe118e3e4049e7bffd08346e
Yumin Kim <bfyu@naver.com>
2023-02-12 18:14:11 +0900

    230212 반복문 연습문제 다시 올리기 -c드라이브

--- 01_Java/a003_control/src/edu/kh/control/loop/practice/ForPractice.java
+++ 01_Java/a003_control/src/edu/kh/control/loop/practice/ForPractice.java
@@ -98,18 +98,105 @@ public class ForPractice {
 
 		if (dan >= 2 && dan <= 9) {
 			for(int i = dan; i <= 9; i++) {
-				System.out.println("===== " + dan + "단 =====");
+				System.out.printf("=====  %d단 ===== \n",i);
                 for (int num = 1 ; num  <= 9 ; num ++) {
-				System.out.printf("%d * %d = %2d \n", dan, num, dan * num);
+				System.out.printf("%d * %d = %2d \n", i, num, i * num);
 			}
 
-		} else
+		} }else
 		System.out.println("2~9 사이 숫자만 입력해주세요.");
-
-
-
 	}
-
-
+    public void practice7() {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("정수입력 : ");
+        int input = sc.nextInt();
+        
+        for (int i = 1; i <= input; i++) {
+            for (int x = 1; x <= i; x++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+ }
+    
+    public void practice8() {
+    
+        Scanner sc = new Scanner(System.in); 
+    
+        System.out.print("정수입력 : ");
+        int input = sc.nextInt();
+    
+        for (int i = input; i >= 1; i--) {
+            for (int x = i; x >= 1; x--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    
+    public void practice9() {
+        Scanner sc = new Scanner(System.in);
+    
+        System.out.print("정수입력 : "); 
+        int input = sc.nextInt();
+    
+        for (int i = 1; i <= input; i++) {
+            for (int x = input; x >= i; x--) {
+                System.out.print(" ");
+            } 
+            for (int p = 1; p <= i; p++) {
+                System.out.print("*");
+                for(int i = dan; i <= 9; i++) {
+                    System.out.printf("=====  %d단 ===== \n",i);
+                    for (int num = 1 ; num  <= 9 ; num ++) {
+                        System.out.printf("%d * %d = %2d \n", i, num, i * num);
+                    }
+                    System.out.println();
+    
+                }
+            }
+            
+              
+        }
+    }
+    
+    public void practice10() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("정수입력 : ");
+        int input = sc.nextInt();
+        
+        for (int i = 0; i < 5; i++) { // 1~5번째 줄을 담당하는 for문
+            for (int j = 0; j < i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < 4; i++) { // 6~9번째 줄을 담당하는 for문
+            for (int j = 0; j < 4 - i; j++) {
+                System.out.print("*");
+            }
+        }            System.out.println();
+    }   
+    public void practice13() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("자연수 하나를 입력하세요 : ");
+        int input = sc.nextInt();
+        
+        int count = 1;
+        for (int i = 1; i <= input; i++) {
+            if (i % 2 == 0 || i % 3 == 0) {
+                System.out.print(i + " ");
+            
+        }                if (i % 2 == 0 && i % 3 == 0)
+            count++;
+        
+    }System.out.println();
+    System.out.printf("count: %d", count);  
+      
+    ddd
 }
+        
+}
+    
 
