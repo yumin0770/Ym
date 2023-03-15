b44c63eecb86076ab8f1a99c42422504143237f4
ymkim <bfyu@naver.com>
2023-03-15 19:48:29 +0900

    230315_DB 및 과제

--- 02_DB/workbook/02_Function.sql
+++ 02_DB/workbook/02_Function.sql
@@ -13,11 +13,11 @@ WHERE PROFESSOR_NAME NOT LIKE '___';
 
 --3.춘 기술대 남자 교수들 이름,나이 출력, 단 나이 순 교수이름, 만나이
 
-SELECT PROFESSOR_NAME 교수이름,  FLOOR((SYSDATE-(TO_DATE(SUBSTR(PROFESSOR_SSN ,1,6)),'RRYYMM'))/365)
-FROM TB_PRSSOR
+SELECT PROFESSOR_NAME 교수이름,  
+       FLOOR(MONTHS_BETWEEN(SYSDATE, TO_DATE('19'||SUBSTR(PROFESSOR_SSN, 1,6),'YYYYMMDD')) / 12 ) 나이
+FROM TB_PROFESSOR 
 WHERE SUBSTR(PROFESSOR_SSN,8,1) = '1'; 
---***********************************************************************
-
+--------------------------------------------------------------
 --4.교수이름 중 성을 제외한 이름만 출력해라
 
 SELECT SUBSTR(PROFESSOR_NAME,2,3)
@@ -29,13 +29,13 @@ FROM TB_PROFESSOR ;
 --19살에 입학면 재수생x
 
 
---***********************************************************************
+
 --6.2020년 크리스마슨 무슨 요일인가?
 
 SELECT TO_CHAR(TO_DATE('20201225','YYYYMMDD'),'DAY') FROM DUAL;
 
---7.TO_DATE(99/10/11, YYMM/DD)
---***********************************************************************
+--7. -------------------------------------
+
 --8.춘기술대학교 2000년도 입학자들은 학번이 A로 시작,2000년도 이전 학번은 학생들의 학번과 이름 보여주는 문장
 
 SELECT STUDENT_NO, STUDENT_NAME 
@@ -91,15 +91,10 @@ ORDER BY STUDENT_NAME ;
 
 --15.학번이 A112113인 김고운 학생의 년도, 학기 별 평점과 년도 별 누적 평점, 총 평점
 
---200601
-
-
 SELECT NVL(SUBSTR(TERM_NO,1,4),'') 년도,NVL(SUBSTR(TERM_NO,5,2),' ') 학기,
 		ROUND(AVG(POINT),1) AS 평점
 FROM TB_GRADE 
 WHERE STUDENT_NO = 'A112113' 
 GROUP BY ROLLUP (SUBSTR(TERM_NO,1,4),SUBSTR(TERM_NO,5,2));
-
-
 
 
