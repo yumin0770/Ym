004e239128fbc50739a33687a56214bfdf952ff9
yumin0770 <122767407+yumin0770@users.noreply.github.com>
2023-04-13 00:39:04 +0900

    230413 JS

--- 04_WebFront/Study/Momentum/app.js
+++ 04_WebFront/Study/Momentum/app.js
@@ -1,3 +1,37 @@
-const title = document.querySelectorAll(".hello h1");
 
-title.innerText = "hi";
+const h1 = document.querySelector("h1");
+
+function clickColor(){
+    h1.style.color='blue';
+}
+
+function handleMouseEnter(){
+    h1.innerText = "mouse is here";
+}
+
+function handleMouseLeave(){
+h1.innerText = "mouse is gone";
+}
+function handleWindowResize(){
+    document.body.style.backgroundColor ="red";
+}
+
+function handleWindowCopy(){
+    alert("No Copy!")
+}
+function handleWindowOffline(){
+    alert("SOS no wifi!");
+}
+
+function handleWindowOnline(){
+    alert("All good");
+}
+
+h1.addEventListener("click",clickColor);
+h1.addEventListener("mouseenter",handleMouseEnter);
+h1.addEventListener("mouseleave",handleMouseLeave);
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);
